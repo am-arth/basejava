5a8307ef4c3a54fbde9934a941f2ff7ca0012a1c
am-arth <72318136+am-arth@users.noreply.github.com>
2020-10-13 10:23:25 +0300

    Add files via upload

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,66 +1,71 @@
-﻿import com.sun.deploy.security.SelectableSecurityManager;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     private int size = 0;
+
     void clear() {
-        for (int j = 0; j < storage.length; j++) {
+        for (int j = 0; j < size; j++) {
             storage[j] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
-          for (int i = 0; i < storage.length; i++) {
-
-              //Сравниваекм введенное значение со значением элемента массива
-              //При соспадении прерываем цикл
-              //Вопрос. Почему не отрабатывает?
-              if (r.equals(storage[i])) {
-                  System.out.println("Такое значение уже существует в массиве");
-                  break;
-              }
-
-              //Следующему элементу со значением NULL присваиваем введенное с клавиатуры.
-              if (storage[i] == null) {
-                  storage[i] = r;
-                  System.out.println(storage[i]);
-                  break;
-              }
-
-        }
-
-    }
-
-    Object get(String uuid) {
-        //Если элемент массива не существует, то должен возвратиться NULL
-        //Вопрос. Если не существует элемент массива, то выдается ошибка.
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i].uuid == uuid) {
-                 return storage[i];
+        int index = -1;
+        if (r.uuid != null) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].uuid.equals(r.uuid)) {
+                    index = i;
+                    if (index > -1) {
+                        break;
+                    }
+                }
             }
         }
-       return null;
+        if (index == -1) {
+            for (int i = 0; i < storage.length; i++) {
+                if (storage[i] == null) {
+                    storage[i] = r;
+                    size++;
+                    break;
+                }
+            }
+        }
+     }
+
+    Object get(String uuid) {
+        int index = -1;
+            for (int i = 0; i < size; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    index = i;
+                    if (index > -1) {
+                        return storage[index];
+                    }
+                }
+            }
+        return null;
     }
 
     void delete(String uuid) {
 
-            for (int i = 0; i < storage.length; i++) {
-             if (storage[i].uuid == uuid) {
-                 storage[i] = null;
-                 break;
-             }
-         }
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid) {
+                storage[i] = null;
+                size--;
+                System.out.println(size);
+                break;
+            }
+        }
         //После присваения элементу массива NULL сдвигаем влево значения
         //чтобы все NULL остались в конце
-        for (int j=0; j<storage.length; j++){
-            if (storage[j]==null){
-                for (int k=j+1; k<storage.length; k++){
-                    storage[k-1] = storage[k];
+        for (int j = 0; j < storage.length; j++) {
+            if (storage[j] == null) {
+                for (int k = j + 1; k < storage.length; k++) {
+                    storage[k - 1] = storage[k];
                 }
-                storage[storage.length-1] = null;
+                storage[storage.length - 1] = null;
                 break;
             }
         }
@@ -70,7 +75,11 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-          return new Resume[0];
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            resumes[i] = storage[i];
+        }
+        return resumes;
     }
 
     int size() {
--- src/MainArray.java
+++ src/MainArray.java
@@ -1,4 +1,4 @@
-﻿import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Console;
@@ -14,10 +14,10 @@ public class MainArray {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Р’РІРµРґРёС‚Рµ РѕРґРЅСѓ РёР· РєРѕРјР°РЅРґ - (list | save uuid | delete uuid | get uuid | clear | exit): ");
+            System.out.print("Введите одну из команд - (list | save uuid | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 2) {
-                System.out.println("РќРµРІРµСЂРЅР°СЏ РєРѕРјР°РЅРґР°.");
+                System.out.println("Неверная команда.");
                 continue;
             }
             String uuid = null;
@@ -26,10 +26,7 @@ public class MainArray {
             }
             switch (params[0]) {
                 case "list":
-                   // printAll();
-                        for (int i = 0; i < ARRAY_STORAGE.storage.length; i++) {
-                         System.out.println(ARRAY_STORAGE.storage[i]);
-                    }
+                    printAll();
                     break;
                 case "size":
                     System.out.println(ARRAY_STORAGE.size());
@@ -54,7 +51,7 @@ public class MainArray {
                 case "exit":
                     return;
                 default:
-                    System.out.println("РќРµРІРµСЂРЅР°СЏ РєРѕРјР°РЅРґР°.");
+                    System.out.println("Неверная команда.");
                     break;
             }
         }
