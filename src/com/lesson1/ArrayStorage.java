453e170c012ac7ef6290f0ac1295bdc58cd51362
Petr@DESKTOP-2Q6SKBR.(none) <pvkarat@gmail.com>
2021-03-15 14:31:59 +0300

    Корректировка метода getAll()

--- src/com/lesson1/ArrayStorage.java
+++ src/com/lesson1/ArrayStorage.java
@@ -43,7 +43,10 @@ public class ArrayStorage {
      */
     public Resume[] getAll() {
         Resume[] result = new Resume[size];
-        return result;
+    	for (int i = 0; i < size; i++) {
+         result[i] = storage[i];
+        }        
+	    return result;
     }
 
     public int size() {
