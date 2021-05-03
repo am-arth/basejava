7ce80d44b56c26a864e2f98bd14c2e76fa714b2b
Petr@DESKTOP-2Q6SKBR.(none) <pvkarat@gmail.com>
2021-05-03 18:51:24 +0300

    Модульное тестирование

---
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,36 @@
+package com.urise.webapp.storage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractArrayStorageTest {
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void size() {
+    }
+}
