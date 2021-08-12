f47c25e84788037a1f5f3a0d90dd6a89e5eb2a1f
Petr@DESKTOP-2Q6SKBR.(none) <pvkarat@gmail.com>
2021-08-12 15:32:32 +0300

    Д.З. 11 Реализация deadlock(корректировка)

--- src/com/urise/webapp/DeadLock.java
+++ src/com/urise/webapp/DeadLock.java
@@ -1,46 +1,29 @@
 package com.urise.webapp;
 
 public class DeadLock {
-   public static Object Lock1 = new Object();
-   public static Object Lock2 = new Object();
 
-   public static void main(String args[]) {
-      Thread1 t1 = new Thread1();
-      Thread2 t2 = new Thread2();
-      t1.start();
-      t2.start();
-   }
+    public static void main(String args[]) {
+        String lock1 = "lock1";
+        String lock2 = "lock2";
+        run(lock1, lock2);
+        run(lock2, lock1);
+    }
 
-   private static class Thread1 extends Thread {
-      public void run() {
-         synchronized (Lock1) {
-            System.out.println("Holding lock 1");
-            try {
-               sleep(500);
-            } catch (InterruptedException e) {
-               e.printStackTrace();
+    private static void run(Object lock1, Object lock2) {
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " Waiting " + lock1.toString());
+            synchronized (lock1) {
+                System.out.println(Thread.currentThread().getName() + " Holding " + lock1.toString());
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " Waiting " + lock2.toString());
+                synchronized (lock2) {
+                    System.out.println(Thread.currentThread().getName() + " Holding " + lock2.toString());
+                }
             }
-            System.out.println("Waiting lock 2");
-            synchronized (Lock2) {
-               System.out.println("Holding lock 2");
-            }
-         }
-      }
-   }
-   private static class Thread2 extends Thread {
-      public void run() {
-         synchronized (Lock2) {
-            System.out.println("Holding lock 2");
-            try {
-               sleep(500);
-            } catch (InterruptedException e) {
-               e.printStackTrace();
-            }
-            System.out.println("Waiting lock 1");
-            synchronized (Lock1) {
-               System.out.println("Holding lock 1");
-            }
-         }
-      }
-   } 
+        }).start();
+    }
 }
