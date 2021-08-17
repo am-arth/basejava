0c85cef505da790935123307bbd7050961d5b977
Petr@DESKTOP-2Q6SKBR.(none) <pvkarat@gmail.com>
2021-08-17 18:13:09 +0300

    Д.З. 12 реализация MainStream корректировка

--- src/com/urise/webapp/MainStream.java
+++ src/com/urise/webapp/MainStream.java
@@ -16,11 +16,16 @@ public class MainStream {
     }
 
     private static int minValue(int[] values) {
-        return Arrays.stream(values).distinct().sorted().reduce(0, (x, y) -> 10 * x + y);
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (x, y) -> 10 * x + y);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
         int mod = integers.stream().mapToInt(Integer::intValue).sum() % 2;
-        return integers.stream().filter(n -> n % 2 != mod).collect(Collectors.toList());
+        return integers.stream()
+                .filter(n -> n % 2 != mod)
+                .collect(Collectors.toList());
     }
 }
